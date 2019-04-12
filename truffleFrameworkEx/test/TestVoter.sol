0d817b9051e7e125dd41f01ff592e1e32d19b3b9
Richard Gordon <richgordon95@gmail.com>
2019-04-12 13:31:02 -0400

    remove temp files and add solidity Testing example

--- truffleFrameworkEx/test/TestVoter.sol
+++ truffleFrameworkEx/test/TestVoter.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.5.0;
 
-import "truffle/Assert.sol";
-import "../contracts/Voter.sol";
+import "truffle/Assert.sol"; // import Assert from truffle
+import "../contracts/Voter.sol"; // import contract you wish to test
 
 contract TestVoter {
     function testVoteForAnOptionUsingNumericIndex() public {
@@ -13,7 +13,7 @@ contract TestVoter {
         voter.vote(0);
         uint[] memory votes = voter.getVotes();
         uint[] memory expected = new uint[](2);
-        expected[0] = 1;
+        expected[0] = 1; // will fail currently if you change this number from 1 to 0 or 2 
         expected[1] = 0;
         Assert.equal(votes, expected, "First option should be voted for");
     }
