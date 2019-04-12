1b643eaa66273c178c1824ccbf763685f3193bb2
Richard Gordon <richgordon95@gmail.com>
2019-04-12 00:20:43 -0400

    unit test comments

--- truffleFrameworkEx/test/voter.js
+++ truffleFrameworkEx/test/voter.js
@@ -1,13 +1,19 @@
 let Voter = artifacts.require('./Voter.sol');
+// most tests have similar pattern
+// 1) create contract
+// 2) interact with it
+// 3) verify state is what you expected
 
+// reference the contract by name
 contract('Voter', function(accounts) {
 
     let voter;
     let firstAccount;
 
+    //use beforeEach to prevent duplicated code
     beforeEach(async function() {
         firstAccount = accounts[0];
-        voter = await Voter.new();
+        voter = await Voter.new(); // create new instance of your contract in beforeEach so each test runs independently
         await setOptions(firstAccount, ['coffee', 'tea'])
     });
 
@@ -58,3 +64,30 @@ contract('Voter', function(accounts) {
         })
     }
 });
+
+
+// other notes:
+// this file uses "expect" style assertions from Chai, but you can use any of your choice
+// if unfamiliar with expect style assertions view below example (unrelated to this file)
+/*
+    let num 1 + 1; // this is what you are testing below
+    expect(num).to.equal(2); // this will pass 
+    expect(num).to.equal(3); // this will fail    
+    expect(num).to.equal(11); // this will fail
+    expect(num).to.equal(42); // this will fail
+
+ */
+// web3.js does not use normal JavaScript numbers, but BigNumbers, so for web3 the below will fail:
+/*
+    var num = await contract.getNum.call({from: accounts[0]});
+
+    expect(num).to.equal(1); // this fails
+    
+    // the below would pass
+    expect(num).to.deep.equal(
+        Contract.web3.toBigNumber(1);
+    );
+    
+    // this also would pass and is much more straightforward
+    expect(num.toNumber()).to.equal(1);
+*/
